3e2128686798cf616524e4a855ed9b4b64612fd9
jiyounggo <103088450+jiyounggo@users.noreply.github.com>
2022-06-10 11:03:06 +0900

    revise1

--- src/component/search_header/search_header.js
+++ src/component/search_header/search_header.js
@@ -1,56 +1,43 @@
-import styles from './search_header.module.css'
-import React, { useRef, useState } from 'react';
-import styled, {css} from 'styled-components';
+import styles from './search_header.module.css';
+import React, { useRef } from 'react';
 
-const Header = styled.div`
-  display : flex;
-  height : 4rem;
-  padding : 0.8em 1em;
-  background: black;
-  color : white;
-`
-const LogoIMG= styled.img`
-`
-const Title= styled.h1`
-margin: 0;
-margin-right:1.1em
-`
-const Input= styled.input`
- flex-basis : 100%
- outline : 0;
- font-size : 1.2rem
-`
-const Button= styled.button`
- backgroung-color : darkgrey;
-`
-const ButtonIMG= styled.img`
- padding : 0.2em;
- height:100%
-
-`
-const SearchHeader = ({onSearch}) => {
-    let [입력값,입력값변경]=useState("");
-    
-    const onSubmit =(e)=>{
-      e.preventDefault();
-      입력값변경("")
-      onSearch(입력값)
-
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
   };
-    return(
-    <Header>   
-       <LogoIMG src = "/images/logo.png" alt="logo"/>
-        <Title>Youtube</Title>
-        <form>
-        <Input value={입력값} type="search" placeholder="search..." onChange={(e)=>{
-          입력값변경(e.currentTarget.value)
-        }} />
-        <Button type="submit" onClick={(e)=>onSubmit(e)}>
-        <ButtonIMG src ="/images/search.png" alt="search"/>
-        </Button>
-        </form>
-    </Header>
-    )
-  }
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
+        <img
+          className={styles.buttonImg}
+          src="/images/search.png"
+          alt="search"
+        />
+      </button>
+    </header>
+  );
+};
 
 export default SearchHeader;
