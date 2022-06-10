481c8a586831cd194a12d51396636776e1cd83fb
jiyounggo <103088450+jiyounggo@users.noreply.github.com>
2022-06-10 11:29:23 +0900

    navbar revise2

--- src/component/search_header/search_header.js
+++ src/component/search_header/search_header.js
@@ -1,43 +1,67 @@
-import styles from './search_header.module.css';
-import React, { useRef } from 'react';
+import styles from './search_header.module.css'
+import React, { useRef, useState } from 'react';
+import styled, {css} from 'styled-components';
 
-const SearchHeader = ({ onSearch }) => {
-  const inputRef = useRef();
-  const handleSearch = () => {
-    const value = inputRef.current.value;
-    onSearch(value);
-  };
-  const onClick = () => {
-    handleSearch();
-  };
+const Header = styled.div`
+  display : flex;
+  height : 4rem;
+  padding : 0.8em 1em;
+  background: black;
+  color : white;
+`
+const LogoIMG= styled.img`
+`
+const Form= styled.form`
 
-  const onKeyPress = event => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
+flex-basis: 100%;
+display:flex;
+`
+const Title= styled.h1`
+margin: 0;
+margin-right:1.1em
+`
+const Input= styled.input`
+ outline : 0;
+ font-size : 1.2rem
+ text-align:center
+
+ display: inline-block;
+ width:100%
+ 
+`
+const Button= styled.button`
+ backgroung-color : darkgrey;
+ outline: 0;
+
+`
+const ButtonIMG= styled.img`
+ padding : 0.2em;
+ height:1.4em;
+
+`
+const SearchHeader = ({onSearch}) => {
+    let [입력값,입력값변경]=useState("");
+    
+    const onSubmit =(e)=>{
+      e.preventDefault();
+      입력값변경("")
+      onSearch(입력값)
+
   };
-  return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
-        <h1 className={styles.title}>Youtube</h1>
-      </div>
-      <input
-        ref={inputRef}
-        className={styles.input}
-        type="search"
-        placeholder="Search..."
-        onKeyPress={onKeyPress}
-      />
-      <button className={styles.button} type="submit" onClick={onClick}>
-        <img
-          className={styles.buttonImg}
-          src="/images/search.png"
-          alt="search"
-        />
-      </button>
-    </header>
-  );
-};
+    return(
+    <Header>   
+      <LogoIMG src = "/images/logo.png" alt="logo"/>
+        <Title>Youtube</Title>
+        <Form>
+      <Input value={입력값} type="search" placeholder="search..." onChange={(e)=>{
+          입력값변경(e.currentTarget.value)
+        }} />
+      <Button type="submit" onClick={(e)=>onSubmit(e)}>
+        <ButtonIMG src ="/images/search.png" alt="search"/>
+        </Button>
+        </Form>
+    </Header>
+    )
+  }
 
 export default SearchHeader;
