5c92b4bbc78e9641065586709f03d9d498a36beb
jiyounggo <103088450+jiyounggo@users.noreply.github.com>
2022-06-10 10:32:13 +0900

    iframe

--- src/component/video_list/video_list.js
+++ src/component/video_list/video_list.js
@@ -2,21 +2,16 @@ import React from 'react';
 import VideoItem from '../video_item/video_item';
 import styles from './video_list.module.css'
 
-// const VideoList = (props,videos) => (
-//   <ul className={styles.videos}> 
-//     {props.videos.map(video => (
-//       <VideoItem key={video.id} video={video} />
-//     ))}
-//   </ul>
-// );
-
-// export default VideoList;
 
 
-const VideoList = ({videos,onVideoClick}) => (
+const VideoList = ({videos,onVideoClick,display}) => (
   <ul className={styles.videos}> 
     {videos.map(video => (
-      <VideoItem key={video.id} video={video} onVideoClick={onVideoClick}/>
+      <VideoItem 
+      key={video.id} 
+      video={video} 
+      onVideoClick={onVideoClick}
+      display={display}/>
     ))}
   </ul>
 );
